161e19d32f37108851d65c1aad28e8b34b03e80a
Joe Haines <hello@joehaines.co.uk>
2017-04-03 18:50:12 +0100

    Rename confusing variable name

--- lib/block.lua
+++ lib/block.lua
@@ -2,25 +2,25 @@ local colours = require 'lib/colours'
 local config = require 'lib/config'
 
 local createBlock = function(x, y)
-  local self = {
+  local block = {
     colourIndex = 1,
     x = x,
     y = y,
   }
 
   local onLeftClick = function()
-    self.colourIndex = self.colourIndex < #colours and self.colourIndex + 1 or 1
+    block.colourIndex = block.colourIndex < #colours and block.colourIndex + 1 or 1
   end
 
   local onRightClick = function()
-    self.colourIndex = self.colourIndex > 1 and self.colourIndex - 1 or #colours
+    block.colourIndex = block.colourIndex > 1 and block.colourIndex - 1 or #colours
   end
 
   local draw = function(startX, startY)
-    local offsetX = startX + self.x * config.squareSize
-    local offsetY = startY + self.y * config.squareSize
+    local offsetX = startX + block.x * config.squareSize
+    local offsetY = startY + block.y * config.squareSize
 
-    local colour = colours[self.colourIndex]
+    local colour = colours[block.colourIndex]
 
     love.graphics.setColor(colour)
     love.graphics.rectangle(
